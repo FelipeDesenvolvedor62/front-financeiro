454ef077dde19c6b0a563f779ee4196d059dc30c
Felipe Alves S <felipe.a.desenvolvedor62@gmail.com>
2022-04-14 12:25:11 -0300

    Adicionado as opções de crud no arquivo service

--- src/app/pages/categorias/shared/categoria.service.ts
+++ src/app/pages/categorias/shared/categoria.service.ts
@@ -8,10 +8,65 @@ import { Categoria } from './categoria.model';
   providedIn: 'root'
 })
 export class CategoriaService {
+
   private apiPath: string = "api/categorias"
 
-  // getAll(): Observable<string> {
-  //   return this.http.get(this.apiPath).pipe();
-  // }
   constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Categoria[]> {
+    return this.http.get(this.apiPath).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategorias)
+    );
+  }
+
+  getId(id: number): Observable<Categoria> {
+    const url = `${this.apiPath}/${id}`;
+
+    return this.http.get(url).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategoria)
+    );
+  }
+
+  create(categoria: Categoria): Observable<Categoria> {
+    return this.http.post(this.apiPath, categoria).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategoria)
+    )
+  }
+
+  update(categoria: Categoria): Observable<Categoria> {
+    const url = `${this.apiPath}/${categoria.id}`;
+
+    return this.http.put(url, categoria).pipe(
+      catchError(this.handleError),
+      map(() => categoria)
+    )
+  }
+
+  delete(id: number): Observable<any> {
+    const url = `${this.apiPath}/${id}`;
+
+    return this.http.delete(url).pipe(
+      catchError(this.handleError),
+      map(() => null)
+    )
+  }
+
+  // PRIVATE METHODS
+  private jsonDataToCategorias(jsonData: any[]): Categoria[] {
+    const categorias: Categoria[] = [];
+    jsonData.forEach(_ => categorias.push(_ as Categoria))
+    return categorias;
+  }
+
+  private jsonDataToCategoria(jsonData: any): Categoria {
+    return jsonData as Categoria;
+  }
+
+  private handleError(error: any): Observable<any> {
+    console.log('Erro na requisição => ', error);
+    return throwError(() => new Error('error'))
+  }
 }
