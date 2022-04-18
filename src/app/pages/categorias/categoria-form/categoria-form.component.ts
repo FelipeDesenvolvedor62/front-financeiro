c22c8f106de4a43eae7b8d221e26c7840ac8c2be
Felipe Alves S <felipe.a.desenvolvedor62@gmail.com>
2022-04-17 23:34:34 -0300

    Importação para controle de formulario

--- src/app/pages/categorias/categoria-form/categoria-form.component.ts
+++ src/app/pages/categorias/categoria-form/categoria-form.component.ts
@@ -1,13 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, Component, OnInit  } from '@angular/core';
+import { FormGroup, FormControl, Validator, FormBuilder } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Categoria } from '../shared/categoria.model';
+import { CategoriaService } from '../shared/categoria.service';
+
+import { switchMap } from 'rxjs';
+
+//import  toastr from 'toastr';
 
 @Component({
   selector: 'app-categoria-form',
   templateUrl: './categoria-form.component.html',
   styleUrls: ['./categoria-form.component.scss']
 })
-export class CategoriaFormComponent implements OnInit {
+export class CategoriaFormComponent implements OnInit, AfterContentChecked {
 
   constructor() { }
+
+  ngAfterContentChecked(){
+
+  }
 
   ngOnInit(): void {
   }
